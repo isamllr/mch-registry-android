f12285569f8969c474ae504ba15c0c3ad295e252
isamllr <isabel.mueller@gmail.com>
2014-08-06 02:02:37 +0200

    call facility

--- mch_client/src/main/java/com/mch/registry/ccs/app/HomeFragment.java
+++ mch_client/src/main/java/com/mch/registry/ccs/app/HomeFragment.java
@@ -1,16 +1,21 @@
 package com.mch.registry.ccs.app;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.mch.registry.ccs.data.Pregnancy;
 import com.mch.registry.ccs.data.PregnancyDataHandler;
 
 public class HomeFragment extends Fragment {
+
+	TextView textFacilityPhone;
 
 	public HomeFragment(){}
 	
@@ -26,14 +31,27 @@ public class HomeFragment extends Fragment {
 		TextView textPatientPhone = (TextView)rootView.findViewById(R.id.textPatientPhoneValue);
 		TextView textExpectedDelivery = (TextView)rootView.findViewById(R.id.textExpectedDeliveryValue);
 		TextView textFacilityName = (TextView)rootView.findViewById(R.id.textFacilityNameValue);
-		TextView textFacilityPhone = (TextView)rootView.findViewById(R.id.textFacilityPhoneValue);
+		textFacilityPhone = (TextView)rootView.findViewById(R.id.textFacilityPhoneValue);
 
 		textPatientName.setText(preg.get_patientName());
 		textPatientPhone.setText(preg.get_mobileNumber());
 		textExpectedDelivery.setText(preg.get_expectedDelivery());
 		textFacilityName.setText(preg.get_facilityName());
 		textFacilityPhone.setText(preg.get_facilityPhoneNumber());
+
+		final Button button = (Button) rootView.findViewById(R.id.button);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				callFacility();
+			}
+		});
          
         return rootView;
     }
+
+	private void callFacility(){
+		Intent callIntent = new Intent(Intent.ACTION_CALL);
+		callIntent.setData(Uri.parse("tel:" + textFacilityPhone.getText().toString().trim()));
+		startActivity(callIntent);
+	}
 }
