7db23711573f9d1827a75c83c14cafbc8c23168d
isamllr <isabel.mueller@gmail.com>
2014-08-06 11:53:29 +0200

    about links clickable

--- mch_client/src/main/java/com/mch/registry/ccs/app/AboutFragment.java
+++ mch_client/src/main/java/com/mch/registry/ccs/app/AboutFragment.java
@@ -3,6 +3,7 @@ package com.mch.registry.ccs.app;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,7 @@ public class AboutFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 		TextView textAbout = (TextView)rootView.findViewById(R.id.about_text);
 		textAbout.setText(Html.fromHtml(getString(R.string.common_about_text)));
+		textAbout.setMovementMethod(LinkMovementMethod.getInstance());
          
         return rootView;
     }
